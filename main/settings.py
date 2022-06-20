bb75af4c8a3e45deeea153c20aa2bf09df472ca0
aliseslikaya <aliseslikaya@gmail.com>
2022-06-20 22:16:32 +0300

    v2

--- main/settings.py
+++ main/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     #Third party apps:
     'rest_framework',
     'drf_yasg',
+    "debug_toolbar",
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [  
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+"127.0.0.1",
+]
